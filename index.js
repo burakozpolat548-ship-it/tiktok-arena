40ac18edee5a085f69d8723df9eb2e524b5104cb
burakozpolat548-ship-it <burakozpolat548@gmail.com>
2026-02-28 17:45:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ app.post("/webhook", (req, res) => {
 app.get("/teams", (req, res) => {
   res.json(teams)
 })
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html")
+})
 app.listen(process.env.PORT || 3000)
